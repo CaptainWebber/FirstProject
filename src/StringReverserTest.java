250453065e6e90f9501e0a58231fc003e13d861a
Captain Webber <Captain.Webber@gmail.com>
2015-06-18 15:29:06 -0700

    Added test

--- src/StringReverserTest.java
+++ src/StringReverserTest.java
@@ -1,10 +1,11 @@
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class StringReverserTest {
     @Test
     public void testReverseString() throws Exception {
-        String solution = new StringReverser().reverseString("abc");
-        Assert.assertEquals("cba", solution);
+        StringReverser sr = new StringReverser();
+        Assert.assertEquals("cba", sr.reverseString("abc"));
+        Assert.assertEquals("rqp1", sr.reverseString("1pqr"));
     }
 }
